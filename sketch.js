d58b2903a70d19b1829c531610dc4f97c4d2e07c
Fabriciofkt157 <99661346+Fabriciofkt157@users.noreply.github.com>
2023-02-12 17:24:05 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,9 +1,13 @@
 function setup (){
-  createCanvas(displayWidth, displayHeight);
+  createCanvas(windowWidth, windowHeight);
+  pixelDensity(100.0);
 }
 function draw (){
   background(110);
+  text (windowWidth, 10, 10);
+  text (windowHeight, 40, 10);
 }
 function mouseClicked (){
   fullscreen(!fs);
 }
+
