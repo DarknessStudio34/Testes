d59b2db3828d4643ecf6342778fdccfb55b61529
Fabriciofkt157 <99661346+Fabriciofkt157@users.noreply.github.com>
2023-02-12 17:51:32 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,9 +1,13 @@
-var pd = pixelDensity ();
+let pd = pixelDensity(); // recupera o valor de pixelDensity()
+
+ // exibe o valor de pixelDensity() no console
+
 function setup (){
   createCanvas(windowWidth, windowHeight);
   pixelDensity()
 }
 function draw (){
+  console.log(pd);
   background(110);
   text (canvas.width, 10, 10);
   text (canvas.height, 40, 10);
