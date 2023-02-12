25ce5708c85cc27001260a2fd389d5d37b5993f8
Fabriciofkt157 <99661346+Fabriciofkt157@users.noreply.github.com>
2023-02-12 16:19:01 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,21 +1,6 @@
 function setup (){
-  createCanvas(w, h)
+  createCanvas(windowWidth, windowHeight);
 }
-var w = windowWidth, h = windowHeight
-
 function draw (){
-  background(220);
-  if (s == 1){
-    w = displatWidth; h = displayHeight; text('display', 10, 10)
-  }
-  else {
-    w = windowWidth; h = windowHeight; text('screen', 10, 10)
-  }
-}
-var s = 0;
-function mouseClicked (){
-  s = 1
-  if (s == 1){
-    s = 0
-  }
+  background(110);
 }
