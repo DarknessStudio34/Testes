d48078f4e1e03987e7fde9fe7d25b97277fb3107
Fabriciofkt157 <99661346+Fabriciofkt157@users.noreply.github.com>
2023-02-12 22:20:59 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -11,6 +11,25 @@ function draw (){
 }
 var x = canvas.width, y = canvas.height
 function mouseClicked (){
-  fullscreen();
+  toggleFullScreen();
 }
-
+function toggleFullScreen() {
+  if ((document.fullScreenElement && document.fullScrenElement !== null) ||    
+   (!document.mozFullScreen && !document.webkitIsFullScreen)) {
+    if (document.documentElement.requestFullScreen) {  
+      document.documentElement.requestFullScreen();  
+    } else if (document.documentElement.mozRequestFullScreen) {  
+      document.documentElement.mozRequestFullScreen();  
+    } else if (document.documentElement.webkitRequestFullScreen) {  
+      document.documentElement.webkitRequestFullScreen(Element.ALLOW_KEYBOARD_INPUT);  
+    }  
+  } else {  
+    if (document.cancelFullScreen) {  
+      document.cancelFullScreen();  
+    } else if (document.mozCancelFullScreen) {  
+      document.mozCancelFullScreen();  
+    } else if (document.webkitCancelFullScreen) {  
+      document.webkitCancelFullScreen();  
+    }  
+  } 
+} 
