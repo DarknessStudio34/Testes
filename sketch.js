ac7150824634531933bd8a5fbd045844314b1db9
Fabriciofkt157 <99661346+Fabriciofkt157@users.noreply.github.com>
2023-02-12 22:03:46 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -6,7 +6,9 @@ function draw (){
   background(110);
   text (canvas.width, 10, 10);
   text (canvas.height, 40, 10);
+  rect(canvas.width, 1, 0, canvas.height );
 }
+var x = canvas.width, y = canvas.height
 function mouseClicked (){
   fullscreen(!fs);
 }
